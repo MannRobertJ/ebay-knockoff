75f1dd848b705ae9f28fb850e9ab77f4e29ca108
MannRobertJ <mann.robertj@gmail.com>
2019-02-13 10:24:53 +0100

    initial entity made

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,24 @@
 import "reflect-metadata";
-import { createKoaServer } from "routing-controllers";
-import Controller from "./controller";
-
-const port = process.env.PORT || 4000;
+import { createKoaServer, Action } from "routing-controllers";
+// import PageController from "./pages/controller";
+// import UserController from "./users/controller";
+import setupDb from "./db";
+// import { verify } from "./logins/jwt";
+// import LoginController from "./logins/controller";
 
 const app = createKoaServer({
-  controllers: [Controller]
-});
+  controllers: []
+  /* authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    if (header && header.startsWith("Bearer ")) {
+      const [, token] = header.split(" ");
+      return !!(token && verify(token));
+    }
+    return false;
+  } */
+});
+const port = process.env.PORT || 4000;
+setupDb()
+  .then(_ => app.listen(port, () => console.log(`Listening on port ${port}`)))
+  .catch(err => console.error(err));
